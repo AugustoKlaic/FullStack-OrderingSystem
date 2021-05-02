10bf2d09f009f768ddf95150108efac0b4219d0d
AugustoKlaic <gutosopelsa@hotmail.com>
2021-05-02 02:00:16 -0300

    adjusting

--- Backend/src/main/java/com/augusto/backend/resource/CategoryHandler.java
+++ Backend/src/main/java/com/augusto/backend/resource/CategoryHandler.java
@@ -2,7 +2,6 @@ package com.augusto.backend.resource;
 
 import com.augusto.backend.domain.Category;
 import com.augusto.backend.dto.CategoryDto;
-import com.augusto.backend.resource.exception.WebException;
 import com.augusto.backend.resource.validator.RequestValidator;
 import com.augusto.backend.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +19,12 @@ public class CategoryHandler {
 
     private static final String CATEGORY_URI = "/categories/";
     private final CategoryService categoryService;
+    private final RequestValidator requestValidator;
 
     @Autowired
-    public CategoryHandler(CategoryService categoryService) {
+    public CategoryHandler(CategoryService categoryService, RequestValidator requestValidator) {
         this.categoryService = categoryService;
+        this.requestValidator = requestValidator;
     }
 
     public Mono<ServerResponse> getCategories(ServerRequest serverRequest) {
@@ -34,37 +35,26 @@ public class CategoryHandler {
 
     public Mono<ServerResponse> getCategoriesById(ServerRequest serverRequest) {
         return Mono.fromCallable(() -> categoryService.findById(Integer.parseInt(serverRequest.pathVariable("id"))))
-                .flatMap(category -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(category))
-                .onErrorResume(e -> ServerResponse.status(HttpStatus.BAD_REQUEST)
-                        .bodyValue(new WebException(HttpStatus.NOT_FOUND, e.getMessage())));
+                .flatMap(category -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(category));
 
     }
 
     public Mono<ServerResponse> createCategory(ServerRequest serverRequest) {
-        return RequestValidator.validateRequest(serverRequest, CategoryDto.class)
+        return serverRequest.bodyToMono(CategoryDto.class)
+                .doOnNext(requestValidator::validateRequest)
                 .map(categoryService::create)
                 .flatMap(createdCategory -> ServerResponse.created(
                         URI.create(CATEGORY_URI.concat(String.valueOf(createdCategory.getId()))))
-                        .bodyValue(createdCategory))
-                .onErrorResume(this::errorHandler);
-
+                        .bodyValue(createdCategory));
     }
 
     public Mono<ServerResponse> updateCategory(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(Category.class).map(categoryService::update)
-                .flatMap(updatedCategory -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(updatedCategory))
-                .onErrorResume(e -> ServerResponse.status(HttpStatus.NOT_FOUND)
-                        .bodyValue(new WebException(HttpStatus.NOT_FOUND, e.getMessage())));
+                .flatMap(updatedCategory -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(updatedCategory));
     }
 
     public Mono<ServerResponse> deleteCategoryById(ServerRequest serverRequest) {
         return Mono.fromCallable(() -> categoryService.deleteById(Integer.parseInt(serverRequest.pathVariable("id"))))
-                .flatMap(categoryId -> ServerResponse.ok().bodyValue(categoryId))
-                .onErrorResume(e -> ServerResponse.status(HttpStatus.BAD_REQUEST)
-                        .bodyValue(new WebException(HttpStatus.NOT_FOUND, e.getMessage())));
-    }
-
-    private Mono<ServerResponse> errorHandler(final Throwable error) {
-        return ServerResponse.status(HttpStatus.BAD_REQUEST).bodyValue(new WebException(HttpStatus.BAD_REQUEST, error.getMessage()));
+                .flatMap(categoryId -> ServerResponse.ok().bodyValue(categoryId));
     }
 }
--- Backend/src/main/java/com/augusto/backend/resource/validator/RequestValidator.java
+++ Backend/src/main/java/com/augusto/backend/resource/validator/RequestValidator.java
@@ -1,35 +1,28 @@
 package com.augusto.backend.resource.validator;
 
-import com.augusto.backend.resource.exception.WebException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import reactor.core.publisher.Mono;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
+import javax.validation.ValidationException;
 import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RequestValidator {
 
-    public static <T> Mono<T> validateRequest(final ServerRequest request, final Class<T> clazz) {
-        return request.bodyToMono(clazz)
-                .flatMap(body -> {
-                    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-                    Validator validator = validatorFactory.getValidator();
+    private final Validator validator;
 
-                    Set<ConstraintViolation<T>> violations = validator.validate(body);
+    public RequestValidator(Validator validator) {
+        this.validator = validator;
+    }
 
-                    if (violations == null || violations.isEmpty()) {
-                        return Mono.just(body);
-                    } else {
-                        return Mono.error(new WebException(
-                                HttpStatus.BAD_REQUEST,
-                                violations.toString()));
-                    }
-                });
+    public void validateRequest(final Object requestObject) {
+        List<String> validationErrors = this.validator.validate(requestObject)
+                .stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
+
+        if (!validationErrors.isEmpty()) {
+            throw new ValidationException();
+        }
     }
 }
