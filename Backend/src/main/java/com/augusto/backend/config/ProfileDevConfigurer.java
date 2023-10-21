09a241d8efb36995acab882a4b22246534d177a7
AugustoKlaic <gutosopelsa@hotmail.com>
2023-10-21 15:17:56 -0300

    Adjust dev profileConfigurer

--- Backend/src/main/java/com/augusto/backend/config/ProfileDevConfigurer.java
+++ Backend/src/main/java/com/augusto/backend/config/ProfileDevConfigurer.java
@@ -2,6 +2,7 @@ package com.augusto.backend.config;
 
 import com.augusto.backend.config.service.CreateTestDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,8 +12,12 @@ import java.text.ParseException;
 @Configuration
 @Profile("dev")
 public class ProfileDevConfigurer {
+    private static final String CREATE_STRATEGY = "create";
 
     private final CreateTestDatabase createTestDatabase;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String databaseCreationStrategy;
 
     @Autowired
     public ProfileDevConfigurer(CreateTestDatabase createTestDatabase) {
@@ -21,6 +26,10 @@ public class ProfileDevConfigurer {
 
     @Bean
     public boolean instantiateDatabase() throws ParseException {
+
+        if(!CREATE_STRATEGY.equals(databaseCreationStrategy)){
+            return false;
+        }
 
         createTestDatabase.instantiateTestDatabase();
         return true;
