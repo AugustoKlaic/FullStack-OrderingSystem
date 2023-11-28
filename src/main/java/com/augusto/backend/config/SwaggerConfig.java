6ec7fc6ccf900e377db9773da0effb2be01291ed
AugustoKlaic <gutosopelsa@hotmail.com>
2023-11-27 22:26:12 -0300

    add security to swagger

--- src/main/java/com/augusto/backend/config/SwaggerConfig.java
+++ src/main/java/com/augusto/backend/config/SwaggerConfig.java
@@ -1,16 +1,34 @@
 package com.augusto.backend.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI springOpenAPI() {
-        return new OpenAPI();
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
+                .info(new Info().title("test").version("v1"));
     }
 
     @Bean
