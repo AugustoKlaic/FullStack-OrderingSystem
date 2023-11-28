1dcd201e85cd7beb98aefa16824e17352dd83df7
AugustoKlaic <gutosopelsa@hotmail.com>
2023-11-28 11:05:54 -0300

    adjusted information on swagger

--- src/main/java/com/augusto/backend/config/SwaggerConfig.java
+++ src/main/java/com/augusto/backend/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.augusto.backend.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -23,7 +25,15 @@ public class SwaggerConfig {
                                 .scheme("Bearer")
                                 .bearerFormat("JWT")))
                 .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
-                .info(new Info().title("Fullstack-OrderingSystem").version("v1"));
+                .info(new Info()
+                        .title("Fullstack-OrderingSystem")
+                        .version("1.0")
+                        .description("Backend APIs")
+                        .contact(new Contact()
+                                .name("Augusto Sopelsa")
+                                .url("https://github.com/AugustoKlaic/FullStack-OrderingSystem"))
+                        .termsOfService("https://www.udemy.com/terms")
+                        .license(new License().name("For studies only")));
     }
 
     @Bean
